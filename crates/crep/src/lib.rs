a4c004fc2fd0bd8eea72ee461d18de5907c37aef
wmurra <william.matthew.murray@gmail.com>
2026-05-03 00:34:36 -0700

    adds message metadata

--- crates/crep/src/lib.rs
+++ crates/crep/src/lib.rs
@@ -4,13 +4,33 @@ pub use errors::DecodeError;
 
 use serde::{Deserialize, Serialize};
 
+// ---- Messages -----
+
 #[derive(Debug, Serialize, Deserialize)]
-pub enum Message {
+pub struct Message {
+    msg_type: MessageType,
+    metadata: MessageMetadata,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MessageMetadata {
+    instance_name: String,
+    user_id: u32
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum MessageType {
     /// Vanity.
     /// tips are basically just our ping.
     TipRequest,
     TipResponse(String),
 }
+
+// ---- Types -----
+
+
+
+// ---- Helpers -----
 
 // bincode serde wrappers
 
