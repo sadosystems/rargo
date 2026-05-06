1c74e361ba95695764e41ff5723e197ada430650
wmurra <william.matthew.murray@gmail.com>
2026-05-05 17:49:35 -0700

    rename cli to abrasive

--- crates/cli/src/bin/abrasive.rs
+++ crates/cli/src/bin/abrasive.rs
@@ -8,12 +8,12 @@ use std::process::ExitCode;
 ///
 /// The agent forwards the message to the broker.
 /// The speed up is only noticeable for cache hits (100ms-1s)
-fn spawn_agent_for_next_time() {
-    todo!("spawn_agent_for_next_time")
-}
+// fn spawn_agent_for_next_time() {
+//     todo!("spawn_agent_for_next_time")
+// }
 
 fn main() -> ExitCode {
-    spawn_agent_for_next_time();
+    // spawn_agent_for_next_time();
     match handle_command() {
         Ok(code) => code,
         Err(e) => e.exit(),
