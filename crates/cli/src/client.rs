c483d1be10889e7c208e62763f731ea6891c29d3
wmurra <william.matthew.murray@gmail.com>
2026-05-07 18:01:44 -0700

    fix abrasive ctx import

--- crates/cli/src/client.rs
+++ crates/cli/src/client.rs
@@ -1,7 +1,8 @@
 use crate::errors::CliError;
+use crate::workspace::AbrasiveContext;
 
 pub type CliResult<T> = Result<T, CliError>;
 
-pub fn command_request(ctx: &AbrasiveContext, cargo_args: Vec<String>) -> {
+pub fn command_request(ctx: &AbrasiveContext, cargo_args: Vec<String>) -> CliResult<()> {
     todo!("")
 }
